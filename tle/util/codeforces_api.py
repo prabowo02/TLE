f9b3f78085f5d00945d6bbd182767e6c07aa4c21
prabowo02 <prabowo1048576@gmail.com>
2024-12-31 14:24:39 +0000

    fix unmagic

--- tle/util/codeforces_api.py
+++ tle/util/codeforces_api.py
@@ -445,55 +445,20 @@ class user:
         return [make_from_dict(Submission, submission_dict) for submission_dict in resp]
 
 
-async def _needs_fixing(handles):
-    to_fix = []
+async def _resolve_handles(handles):
     chunks = user_info_chunkify(handles)
+    resolved_handles = {}
+
     for handle_chunk in chunks:
         while handle_chunk:
-            try:
-                cf_users = await user.info(handles=handle_chunk)
+            cf_users = await user.info(handles=handle_chunk)
 
-                # Users could still have changed capitalization
-                for handle, cf_user in zip(handle_chunk, cf_users):
-                    assert handle.lower() == cf_user.handle.lower()
-                    if handle != cf_user.handle:
-                        to_fix.append(handle)
-                break
-            except HandleNotFoundError as e:
-                to_fix.append(e.handle)
-                handle_chunk.remove(e.handle)
-    return to_fix
-
-
-async def _resolve_redirect(handle):
-    url = 'https://codeforces.com/profile/' + handle
-    async with _session.head(url) as r:
-        if r.status == 200:
-            return handle
-        if r.status == 301 or r.status == 302:
-            redirected = r.headers.get('Location')
-            if '/profile/' not in redirected:
-                # Ended up not on profile page, probably invalid handle
-                return None
-            return redirected.split('/profile/')[-1]
-        raise CodeforcesApiError(
-            f'Something went wrong trying to redirect {url}')
-
-
-async def _resolve_handle_mapping(handles_to_fix):
-    redirections = {}
-    failed = []
-    for handle in handles_to_fix:
-        new_handle = await _resolve_redirect(handle)
-        if not new_handle:
-            redirections[handle] = None
-        else:
-            cf_user, = await user.info(handles=[new_handle])
-            redirections[handle] = cf_user
-    return redirections
+            for handle, cf_user in zip(handle_chunk, cf_users):
+                if handle != cf_user.handle:
+                    resolved_handles[handle] = cf_user.handle
+    return resolved_handles
 
 
 async def resolve_redirects(handles):
-    handles_to_fix = await _needs_fixing(handles)
-    handle_mapping = await _resolve_handle_mapping(handles_to_fix)
-    return handle_mapping
+    """Returns a mapping of handles to their resolved CF users."""
+    return await _resolve_handles(handles)
